69b3cc09a0f780c0ea797cf815f5c85ab8d216ef
Kevin Michael Frick <kmfrick98@gmail.com>
2021-10-29 12:45:22 +0200

    Attempt to fix SGM, still not correct :)

--- uo_nn_v40/uo_sgm.m
+++ uo_nn_v40/uo_sgm.m
@@ -1,24 +1,27 @@
-function [wo, e] = uo_sgm(wo, la, f, g, Xtr, ytr, Xte, yte, alpha, beta, gamma, esg_max, esg_best)
+function [w, e] = uo_sgm(wo, la, f, g, Xtr, ytr, Xte, yte, alpha, beta, gamma, esg_max, esg_best)
 p = size(Xtr, 2);
-m = gamma * p;
-ke = p / m;
-kmax = esg_max * ke;
+m = floor(gamma * p);
+ksge = ceil(p / m);
+ksgmax = esg_max * ksge;
 e = 0;
 s = 0; 
 L_best = 99999;
 k = 0;
 wk = wo;
-while e < esg_max && e < esg_best
-    P =  randperm(p);
+w = wo;
+while e < esg_max && s < esg_best
+    P = randperm(p);
     for i = 1:(int32(ceil(p/m-1)))
         S = P(:, (i*m+1):min((i+1)*m, p));
         Xtrs = Xtr(:, S);
         ytrs = ytr(S);
         dk = -g(wk, Xtrs, ytrs);
-        if k < kmax
-            ak = (1 - k / kmax)*alpha + k / kmax * alpha * 0.01;
+        ksg = floor(beta * ksgmax);
+        alphasg = 0.01 * alpha;
+        if k <= ksg
+            ak = (1 - k / ksg) * alpha + alphasg * k / ksg;
         else
-            ak = 0.01 * alpha;
+            ak = alphasg;
         end
         wk = wk + ak * dk; 
         k = k + 1;
@@ -27,7 +30,7 @@ while e < esg_max && e < esg_best
     Lte = f(wk, Xte, yte);
     if Lte < L_best
         L_best = Lte;
-        wo = wk;
+        w = wk;
         s = 0;
     else
         s = s + 1;
